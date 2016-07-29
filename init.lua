078c4ad7d9c517c512600f2bc91d3fd7b7874bef
acmgit <undertakers_help@yahoo.com>
2016-07-29 21:23:11 +0200

    Billboard balck_yellow added.

--- init.lua
+++ init.lua
@@ -62,3 +62,35 @@ minetest.register_craft({
                                         {"group:stick", "group:stick", "group:stick"}
                                 }
 })
+
+minetest.register_node("billboard:bb_black_yellow", {
+		description = "Billboard black_yellow",
+		drawtype = "signlike",
+                visual_scale = 3,
+		tiles = {
+                                "bb_black_yellow.png"                         -- up
+                            },
+		inventory_image = "bb_black_yellow.png",
+		wield_image = "bb_black_yellow.png",
+		wield_scale = {x=1, y=1, z=1},
+		paramtype = "light",
+		paramtype2 = "wallmounted",
+		sunlight_propagates = true,
+		walkable = false,
+		light_source = 1, -- reflecting a bit of light might be expected
+		selection_box = {
+		type = "wallmounted",
+		wall_side = {-0.45, -1.5, -2.5, -.4, 1.5, 2.5}, -- NodeBox
+		},
+		groups = {choppy=2,dig_immediate=3,attached_node=1},
+		legacy_wallmounted = true,
+
+})
+minetest.register_craft({
+                    output = "billboard:bb_black_yellow",
+                    recipe = {
+                                        {"group:stick", "group:stick", "group:stick"},
+                                        {"default:sign_wall_wood", "wool:black", "wool:yellow"},
+                                        {"group:stick", "group:stick", "group:stick"}
+                                }
+})
