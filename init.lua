e6d5bb8909bcbd8b2ee3d72c339f2aaa78800634
acmgit <undertakers_help@yahoo.com>
2016-07-29 21:23:11 +0200

    Billboard-Nodes changed

--- init.lua
+++ init.lua
@@ -1,23 +1,21 @@
 minetest.register_node("billboard:bb_white_white", {
 		description = "Billboard white_white",
 		drawtype = "signlike",
-                visual_scale = 3,
+                visual_scale = 3.0,
 		tiles = {
-                                "bb_white_white.png"                         -- up
+                                "bb_white_white.png"
                             },
 		inventory_image = "bb_white_white.png",
 		wield_image = "bb_white_white.png",
-		wield_scale = {x=1, y=1, z=1},
 		paramtype = "light",
 		paramtype2 = "wallmounted",
 		sunlight_propagates = true,
 		walkable = false,
 		light_source = 1, -- reflecting a bit of light might be expected
 		selection_box = {
-		type = "wallmounted",
-		wall_side = {-0.45, -1.5, -2.5, -.4, 1.5, 2.5}, -- NodeBox
+                                                type = "wallmounted"
 		},
-		groups = {choppy=2,dig_immediate=3,attached_node=1},
+		groups = {choppy=2,dig_immediate=3,attached_node=1, picture=1},
 		legacy_wallmounted = true,
 
 })
@@ -34,23 +32,21 @@ minetest.register_craft({
 minetest.register_node("billboard:bb_yellow_yellow", {
 		description = "Billboard yellow_yellow",
 		drawtype = "signlike",
-                visual_scale = 3,
+                visual_scale = 3.0,
 		tiles = {
-                                "bb_yellow_yellow.png"                         -- up
+                                "bb_yellow_yellow.png"
                             },
 		inventory_image = "bb_yellow_yellow.png",
 		wield_image = "bb_yellow_yellow.png",
-		wield_scale = {x=1, y=1, z=1},
 		paramtype = "light",
 		paramtype2 = "wallmounted",
 		sunlight_propagates = true,
 		walkable = false,
 		light_source = 1, -- reflecting a bit of light might be expected
 		selection_box = {
-		type = "wallmounted",
-		wall_side = {-0.45, -1.5, -2.5, -.4, 1.5, 2.5}, -- NodeBox
-		},
-		groups = {choppy=2,dig_immediate=3,attached_node=1},
+                                            type = "wallmounted",
+                                            },
+		groups = {choppy=2,dig_immediate=3,attached_node=1, picture=1},
 		legacy_wallmounted = true,
 
 })
@@ -59,6 +55,36 @@ minetest.register_craft({
                     recipe = {
                                         {"group:stick", "group:stick", "group:stick"},
                                         {"default:sign_wall_wood", "wool:yellow", "wool:yellow"},
+                                        {"group:stick", "group:stick", "group:stick"}
+                                }
+})
+
+minetest.register_node("billboard:bb_black_yellow", {
+		description = "Billboard black_yellow",
+		drawtype = "signlike",
+                visual_scale = 3.0,
+		tiles = {
+                                "bb_black_yellow.png"
+                            },
+		inventory_image = "bb_black_yellow.png",
+		wield_image = "bb_black_yellow.png",
+		paramtype = "light",
+		paramtype2 = "wallmounted",
+		sunlight_propagates = true,
+		walkable = false,
+		light_source = 1, -- reflecting a bit of light might be expected
+		selection_box = {
+                                                type = "wallmounted",
+                                            },
+		groups = {choppy=2,dig_immediate=3,attached_node=1, picture=1},
+		legacy_wallmounted = true,
+
+})
+minetest.register_craft({
+                    output = "billboard:bb_black_yellow",
+                    recipe = {
+                                        {"group:stick", "group:stick", "group:stick"},
+                                        {"default:sign_wall_wood", "wool:black", "wool:yellow"},
                                         {"group:stick", "group:stick", "group:stick"}
                                 }
 })
