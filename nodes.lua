ba25a050f3f36d2531536d94bca07baf5d0c5719
acmgit <undertakers_help@yahoo.com>
2016-07-31 12:08:28 +0200

    Army-BB added.

--- nodes.lua
+++ nodes.lua
@@ -6,5 +6,6 @@ bb_boardlist = {
             { "sign_wall_wood", "black", "yellow", 3.5, "jpg" },
             { "sign_wall_wood", "green", "blue", 3.0, "png" },
             { "sign_wall_wood", "blue", "yellow", 2.0, "jpg" },
-            { "paper", "black", "orange", 2.5, "jpg" }
+            { "paper", "black", "orange", 2.5, "jpg" },
+            { "paper", "green", "green", 2.0, "png" }
 }
